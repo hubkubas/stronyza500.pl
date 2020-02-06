0372cdd851a23966bff0ee3a356f928833e88dee
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2020-02-06 22:33:12 +0100

    little changes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
+AOS.init({
+  duration: 1200,
+})
+
+
 $(document).ready(function() {
-
-  /* Sticky navigation */
-  // $('.js--section-features').waypoint(function(direction) {
-  //   if (direction == "down") {
-  //     $('nav').addClass('sticky');
-  //   } else {
-  //     $('nav').removeClass('sticky');
-  //   }
-  // }, {
-  //   offset: '60px;'
-  // });
-
-  /* Scroll on Buttons*/
-  // $('.js--scroll-to-plans').click (function() {
-  //   $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
-  // });
-  //
-  // $('.js--scroll-to-start').click (function() {
-  //   $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-  // });
-
-
+  
   /* Navigation scroll*/
   $('a[href*="#"]')
     // Remove links that don't actually link to anything
@@ -58,46 +42,30 @@ $(document).ready(function() {
       }
     });
 
-    /*Animations os scroll*/
-    // $('.js--wp-1').waypoint(function(directions) {
-    //   $('.js--wp-1').addClass('animated fadeIn');
-    // }, {
-    //   offset: '50%'
-    // });
-    //
-    // $('.js--wp-2').waypoint(function(directions) {
-    //   $('.js--wp-2').addClass('animated fadeInUp');
-    // }, {
-    //   offset: '50%'
-    // });
-    //
-    // $('.js--wp-3').waypoint(function(directions) {
-    //   $('.js--wp-3').addClass('animated fadeIn');
-    // }, {
-    //   offset: '50%'
-    // });
-    //
-    // $('.js--wp-4').waypoint(function(directions) {
-    //   $('.js--wp-4').addClass('animated pulse');
-    // }, {
-    //   offset: '50%'
-    // });
-
-/* Mobile nav */
-// $('.js--nav-icon').click(function(){
-// var nav = $('.js--main-nav');
-// var icon = $('.js--nav-icon i');
-//
-// nav.slideToggle(200);
-// if (icon.hasClass('ion-navicon-round')) {
-// icon.addClass('ion-close-round');
-// icon.removeClass('ion-navicon-round')
-// } else {
-//   icon.addClass('ion-navicon-round');
-//   icon.removeClass('ion-close-round');
-// }
-// });
-
+        /*Animations os scroll*/
+        $('.js--wp-1').waypoint(function(directions) {
+          $('.js--wp-1').addClass('animated fadeIn');
+        }, {
+          offset: '50%'
+        });
+    
+        $('.js--wp-2').waypoint(function(directions) {
+          $('.js--wp-2').addClass('animated fadeInUp');
+        }, {
+          offset: '50%'
+        });
+    
+        $('.js--wp-3').waypoint(function(directions) {
+          $('.js--wp-3').addClass('animated fadeIn');
+        }, {
+          offset: '50%'
+        });
+    
+        $('.js--wp-4').waypoint(function(directions) {
+          $('.js--wp-4').addClass('animated pulse');
+        }, {
+          offset: '50%'
+        });
 
 
 });
